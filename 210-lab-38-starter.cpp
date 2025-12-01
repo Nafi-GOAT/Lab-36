e7fcf77fdd52ab2089c637cb9de42f9866a61fb6
Nafi-GOAT <nafih2989@gmail.com>
2025-11-30 20:34:05 -0800

    All files added and Milestone 1 Complete

---
+++ 210-lab-38-starter.cpp
@@ -0,0 +1,15 @@
+//
+//Mehraj Hasan Nafi || COMSC 210 || Lab 36
+// IDE used : Visual Studio Code 
+//
+//
+#include <iostream>
+#include "IntBinaryTree.h"
+using namespace std;
+
+int main() {
+         IntBinaryTree tree;
+         cout << "This Lab 36 is set and running successfully!" << endl;
+    
+         return 0;
+}
