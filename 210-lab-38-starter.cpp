e6d10243789859cc6893ee47068fe714d67034a0
Nafi-GOAT <nafih2989@gmail.com>
2025-11-30 21:54:17 -0800

    Milestone 4 tested and complete

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main() {
          IntBinaryTree tree;
 
-         cout << "Testing for Milestone 3" << endl; 
+         cout << "Testing for Milestone 4" << endl; 
 
     tree.insertNode("HELLO");
     tree.insertNode("WORLD");
@@ -25,6 +25,27 @@ int main() {
 
 
         cout << "Displaying in Order: " << endl;
+        tree.displayInOrder();
+
+        //test searches
+        cout << "\nSearching for COMSC: ";
+        if(tree.searchNode("COMSC"))
+            cout << "Found!" << endl;
+        else
+            cout << "Not Found!" << endl;
+
+            cout << "\nSearching for TEST: ";
+        if(tree.searchNode("TEST"))
+            cout << "Found!" << endl;
+        else
+            cout << "Not Found!" << endl;   
+            
+            //test deletions
+        cout << "\nRemoving COMSC and LAB from the tree." << endl;
+        tree.remove("COMSC");
+        tree.remove("LAB");
+
+        cout << "\nDisplaying in Order after deletions: " << endl;
         tree.displayInOrder();
 
          return 0;
